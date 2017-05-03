553cd0cf10c2edf5b5e460e76fb2a6576a9d34c9
haroldlam <455727503@qq.com>
2017-05-03 18:19:26 +0800

    First commit

---
+++ src/Sayhello.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/3
+ * Time: 17:58
+ */
+namespace Hello;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World!';
+    }
+}
